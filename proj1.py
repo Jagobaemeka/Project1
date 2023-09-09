a7c912d0ba4118af3baa76daf3fcff736aaa3b8f
G00405014 <G00405014@atu.ie>
2023-09-09 13:05:05 +0100

    Record Player Scores

--- proj1.py
+++ proj1.py
@@ -10,3 +10,17 @@ def roll():
 
 value = roll()
 print(value)
+while True:
+    players = input("Enter the number of players (2-4):")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be berween 2-4 players")
+    else:
+        print("Invalid,try again")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+print(player_scores)
